fcb6a9ef403a5da188b4b045ee6da414872d7fad
Anna <acdodd17@gmail.com>
2022-01-17 09:15:11 -0500

    model associations in models/index.js

--- models/index.js
+++ models/index.js
@@ -3,16 +3,37 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 //User has many posts
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
 
 //Post belongs to User
+Post.belongsTo(User, {
+    foreignKey: 'user_id', 
+    onDelete: 'SET NULL'
+});
 
 //User has many comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id', 
+    onDelete: 'SET NULL'
+});
 
 //Comment belongs to User
+Comment.belongsTo(User, {
+    foreignKey: 'user_id', 
+    onDelete: 'SET NULL'
+});
 
 //Post has many comments
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
 
 //Comment belongs to Post
-
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id', 
+    onDelete: 'SET NULL'
+});
 
 module.exports = { User, Post, Comment };
