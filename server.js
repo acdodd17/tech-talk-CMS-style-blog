ba319d3bc7106936aad33ad1ee7fc2c91ad7647d
Anna <acdodd17@gmail.com>
2022-01-17 14:29:27 -0500

    initial user routes

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use (express.static(path.join(__dirname, 'public')));
+//app.use (express.static(path.join(__dirname, 'public')));
 
 //app.use(session(sess));
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
