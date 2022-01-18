9345ac6ff523a0fa8e41de7d1a315a418b5006e9
Anna <acdodd17@gmail.com>
2022-01-18 10:00:50 -0500

    login and signup forms

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'no one knows',
+  secret: process.env.DB_SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
