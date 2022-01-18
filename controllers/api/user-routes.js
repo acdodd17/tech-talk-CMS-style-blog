bfc146d5d35d2702e1d0efa7857118deb683de42
Anna <acdodd17@gmail.com>
2022-01-18 08:04:58 -0500

    session updates

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -55,7 +55,15 @@ router.post('/', (req, res) => {
       email: req.body.email,
       password: req.body.password,
     })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+      req.session.save( () => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData)
+      });
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
@@ -80,11 +88,28 @@ router.post('/login', (req, res) => {
         res.status(400).json({ message: 'Incorrect password!' });
         return;
       }
-  
-      res.json({ user: dbUserData, message: 'You are now logged in!' });
+      
+      req.session.save( () => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });
     });
 });
 
+// logout user
+router.post ('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy( () => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
+})
 // update user info
 router.put('/:id', (req, res) => {
     User.update(req.body, {
