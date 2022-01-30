85187b73475ce142f3555022c86d7c7930219633
Anna <acdodd17@gmail.com>
2022-01-30 17:41:22 -0500

    edit post content

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3006;
 
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
